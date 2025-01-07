305b60831829f6ffb05f2a6e722e949209ac7771
Andres Taborda <andrestaborda43@gmail.com>
2025-01-07 21:09:04 +0000

    creando modelo professional y añadiendo relaciones

--- src/models/client/client_model.py
+++ src/models/client/client_model.py
@@ -1,6 +1,10 @@
 from models import db
 
 class Client(db.Model):
+    __tablename__ = 'clients'
+
+    fullname = db.Column(db.String(120), nullable = False)
+    
     id = db.Column(db.Interger, primary_key = True)
     id_user = db.Colum(db.Interger, db.ForeingKey("user.id"), nullable = False)
 
